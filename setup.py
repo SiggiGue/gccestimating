b82dc3326cb5979382023c5166455045e781aa26
Siegfried Gündert <siegfried.guendertATuol.de>
2025-09-17 15:09:54 +0200

    Adapt versions.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gccestimating',
-    version='0.2.0',
+    version='0.2.1',
     author='Siegfried Gündert',
     author_email='siegfried.guendert@gmail.com',
     license='MPL-2.0',
